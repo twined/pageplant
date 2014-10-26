6d66e71ee7cb2914f202a55ad9783bd7100be54f
Trond Mjøen <trond@twined.net>
2014-10-26 18:41:01 +0100

    - version bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 
 setup(
     name='pageplant',
-    version='0.1.0',
+    version='1.0.1',
     author=u'Twined',
     author_email='www.twined.net',
     packages=find_packages(),
