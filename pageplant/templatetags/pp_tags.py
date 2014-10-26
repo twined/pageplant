aa62c07040959d15300d1fcaa7290bf220c095cc
Trond Mjøen <trond@twined.net>
2014-10-26 21:31:43 +0100

    - fixing DoesNotExist

--- pageplant/templatetags/pp_tags.py
+++ pageplant/templatetags/pp_tags.py
@@ -15,8 +15,11 @@ class RenderPartialPage(InclusionTag):
     )
 
     def get_context(self, context, slug):
-        page = Page.objects.get(slug=slug)
-        page = page or '<div class="error">Mangler side: %s</div>' % slug
+        try:
+            page = Page.objects.get(slug=slug)
+        except Page.DoesNotExist:
+            page = '<div class="error">Mangler side: %s</div>' % slug
+
         return {'page': page}
 
 register.tag(RenderPartialPage)
